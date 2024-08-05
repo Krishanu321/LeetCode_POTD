35cee86450dc632faf7939e54b26c0f3f629dba6
Krishanu Santra <krishanusantra345@gmail.com>
2024-08-05 23:29:27 +0530

    1508. Range Sum of Sorted Subarray Sums

---
+++ 04.08.2024.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int rangeSum(vector<int> &nums, int n, int left, int right)
+    {
+        vector<int> subarraySums;
+
+        // Generate all subarray sums
+        for (int i = 0; i < n; ++i)
+        {
+            int currentSum = 0;
+            for (int j = i; j < n; ++j)
+            {
+                currentSum += nums[j];
+                subarraySums.push_back(currentSum);
+            }
+        }
+
+        // Sort the subarray sums
+        sort(subarraySums.begin(), subarraySums.end());
+
+        // Calculate the sum of the elements from index left to right
+        int MOD = 1e9 + 7;
+        long long result = 0;
+        for (int i = left - 1; i < right; ++i)
+        {
+            result = (result + subarraySums[i]) % MOD;
+        }
+
+        return static_cast<int>(result);
+    }
+};
